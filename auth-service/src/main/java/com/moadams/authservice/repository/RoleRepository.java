24f0c77fb76b2869344d44dde4c9f1a300d9e47e
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-07-03 17:13:24 +0000

    feat: add repository for role entity

---
+++ auth-service/src/main/java/com/moadams/authservice/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.moadams.authservice.repository;
+
+import com.moadams.authservice.enums.RoleName;
+import com.moadams.authservice.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName name);
+}
