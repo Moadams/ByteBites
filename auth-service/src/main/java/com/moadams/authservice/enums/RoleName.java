b2d85c26ed9e6505dafea11e6ae1198897f06899
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-07-03 15:59:19 +0000

    add validation dependency

--- auth-service/src/main/java/com/moadams/authservice/enums/RoleName.java
+++ auth-service/src/main/java/com/moadams/authservice/enums/RoleName.java
@@ -1,4 +1,9 @@
 package com.moadams.authservice.enums;
 
 public enum RoleName {
+    ROLE_ADMIN,
+    ROLE_MANAGER,
+    ROLE_DEVELOPER,
+    ROLE_CONTRACTOR
 }
+
--- auth-service/src/main/java/com/moadams/authservice/model/User.java
+++ auth-service/src/main/java/com/moadams/authservice/model/User.java
@@ -1,4 +1,92 @@
 package com.moadams.authservice.model;
 
-public class User {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name="users", uniqueConstraints = @UniqueConstraint(name = "unique_user_email", columnNames = "email"))
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Enter a valid email address")
+    @NotBlank(message = "Email must not be blank")
+    @Column(unique=true, nullable = false)
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream().map(role -> new SimpleGrantedAuthority(role.getName().name())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
+
