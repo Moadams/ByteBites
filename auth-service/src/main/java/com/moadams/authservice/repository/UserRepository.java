b5ca695bbff1f6807e15f6a4c543a4e521132ec1
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-07-03 16:13:17 +0000

    feat: implement user repository for interaction with database

---
+++ auth-service/src/main/java/com/moadams/authservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.moadams.authservice.repository;
+
+import com.moadams.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
