0c1c3ef57cb66a9c4afeba39d8444c0b9c15f58c
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-07-08 17:13:10 +0000

    feat: add validations to restaurant entity

--- restaurant-service/src/main/java/com/moadams/restaurantservice/model/Restaurant.java
+++ restaurant-service/src/main/java/com/moadams/restaurantservice/model/Restaurant.java
@@ -1,6 +1,9 @@
 package com.moadams.restaurantservice.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,15 +23,21 @@ public class Restaurant {
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "Name is required")
     private String name;
 
     @Column(nullable = false)
+    @NotBlank(message = "Address cannot be blank")
     private String address;
 
     @Column(nullable = false)
+    @Pattern(regexp = "^\\+?[0-9. ()-]{7,25}$", message = "Invalid phone number format")
+    @NotBlank(message = "Phone number is required")
     private String phone;
 
     @Column(nullable = false)
+    @NotBlank(message = "Owner Email is required")
+    @Email(message = "Must be a valid email address")
     private String ownerEmail;
 
 }
